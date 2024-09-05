7c0c7a35e5c663f5102d1f77fd7e16b718fc2b5f
Sergey_Korchemkin <korchemkinsw@yandex.ru>
2024-09-05 12:50:59 +0300

    Update styled.js

--- src/components/ui/input-address/styled.js
+++ src/components/ui/input-address/styled.js
@@ -3,13 +3,11 @@ import styled from "styled-components";
 export const StyledInputAddress = styled.label`
   margin: 0;
   padding: 0;
-  width: 313px;
-  height: 48px;
-  gap: 0px;
-  border: none;
-  background-color: rgba(246, 246, 246, 1);
   input {
-    padding: 14px 131px 13px 12px;
+    width: 293px;
+    height: 48px;
+    padding: 0px 10px;
     border: solid 1px rgba(0, 0, 0, 0.1);
+    background-color: rgba(246, 246, 246, 1);
   }
 `;
